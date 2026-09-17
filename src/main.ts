39e97d05813d15f52cdba1aa787d98f485a23921
Oleksandr Basiuk <basuksasa@gmail.com>
2026-09-17 19:55:17 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(PORT);
